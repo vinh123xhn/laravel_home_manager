3a3816c9a7525a009f86d876412f47dfc94f8c6b
vinh <vinh456xhn@gmail.com>
2019-01-07 13:58:33 +0700

    form request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Division;
+use App\Service;
+use App\ServiceDetails;
+use App\Theme;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getFormRequest(){
+        $divisions = Division::all();
+        $themes = Theme::all();
+        $services = Service::all();
+        $services_details = ServiceDetails::all();
+        return view('form_request', compact('divisions', 'themes', 'services', 'services_details'));
+    }
 }
