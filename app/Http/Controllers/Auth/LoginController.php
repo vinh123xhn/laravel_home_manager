423dd20197258ff526fd8a4766dff94167fb2f1c
vinh <vinh456xhn@gmail.com>
2019-01-18 15:39:40 +0700

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -34,6 +35,18 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest')->except(['logout', 'logoutUser']);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logoutUser()
+    {
+        Auth::guard('web')->logout();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user/logout', 'Auth\LoginController@logoutUser')->name('user.logout');
 
-Route::get('/admin', 'AdminController@index')->name('admin');
+Route::group(['prefix' => 'admin'], function (){
+    Route::get('/login', 'AuthAdmin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'AuthAdmin\LoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.home');
+    Route::get('/logout', 'AuthAdmin\LoginController@logoutAdmin')->name('admin.logout');
+});
+
